bc25a8f4a6f6f18539aea8653c3ff107b9e51b81
jogaag <josephatufe@gmail.com>
2023-02-18 03:01:43 -0800

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,21 @@
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <unistd.h>
 
-int main(void) {
-  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  const int len = sizeof(message) - 1;  // subtract 1 to exclude the null terminator
-  const ssize_t ret = write(2, message, len);  // write to standard error
-  return (ret == len) ? 1 : 0;  // return 1 if all bytes were written, 0 otherwise
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
 }
