c82a23a2ffc6f5dacbddb031e25617273061031c
jogaag <josephatufe@gmail.com>
2023-02-22 06:58:02 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,14 @@
- -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
