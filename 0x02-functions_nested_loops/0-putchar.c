66be09ac2f92385e451b5d51696c3f6bada34564
jogaag <josephatufe@gmail.com>
2023-02-22 05:52:34 -0800

    pusher

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
+
 /**
- * main - print _putchar
- * by jogaags
+ * main - Print _putchar
  * Return: Always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -17,5 +16,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
+}}
 
