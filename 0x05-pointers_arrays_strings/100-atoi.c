80ddf0189211f4ab5f2c2097af1f7e0d3cc4b495
jogaag <josephatufe@gmail.com>
2023-02-27 09:04:51 -0800

    looking good

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,48 +1,40 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer
- * @s: The string to be converted
- * Return: The first integer in the string
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ *
+ * Return: A integer
  */
 int _atoi(char *s)
 {
-	short sign = 1;
-	double result = 0;
-	int len, digits_seen_count, i, pass, positive, old_sign;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	len = i = digits_seen_count = pass = old_sign = 0;
-	while (pass <= 1)
+	while (s[c])
 	{
-		positive = old_sign != 0 ? old_sign == *(s + len) : *(s + len) == '+';
-		if (*(s + len) == '\0')
+		if (s[c] == 45)
 		{
-			pass++;
-			len = 0;
-			continue;
+			min *= -1;
 		}
-		else if (*(s + len) >= '0' && *(s + len) <= '9')
+
+		while (s[c] >= 48 && s[c] <= 57)
 		{
-			if (pass == 0)
-				digits_seen_count++;
-			else
-				result += (*(s + len) - '0'); (digits_seen_count - i++ - 1);
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
 		}
-		else
+
+		if (isi == 1)
 		{
-			if (pass == 0 && digits_seen_count > 0)
-			{
-				pass++;
-				len -= digits_seen_count;
-				continue;
-			}
-			if ((*(s + len) == '+' || *(s + len) == '-') && digits_seen_count == 0)
-			{
-				sign = positive ? 1 : 0 - 1;
-				old_sign = positive ? '+' : '-';
-			}
+			break;
 		}
-		len++;
+
+		c++;
 	}
-	return (result * sign);
-}
+
+	ni *= min;
+	return (ni);
+}}
