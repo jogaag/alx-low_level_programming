2f51b6c300d742d19ae8022c0b04337a70c4ccd0
jogaag <josephatufe@gmail.com>
2023-03-16 04:33:31 -0700

    nicely done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * _puts_recursion - print string
+ * @s:string
+ * Return:void
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n')
+	}
+}
