4801c8a98ee86fb0eca8518c480d356726476c36
jogaag <josephatufe@gmail.com>
2023-03-16 06:27:47 -0700

    cool

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,12 +6,12 @@
  * Return: int
  */
 
-int factorial(int n);
+int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
 	else
-		return (n * (n - 1)!);
+		return (n * factorial(n - 1));
 }
